34138049067e37a375e62d4f7ed13b7a51c35493
Michael Walker <michaelwalker@Michaels-MacBook-Pro.local>
2025-11-19 17:12:35 -0800

    Update TRI EPA table DDL with state_cnty_fips_cd

--- bigquery/ddl/tri_epa.sql
+++ bigquery/ddl/tri_epa.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `rdm-datalab-portfolio.portfolio_data.tri_epa`
 (
-  state_cd STRING,
-  cnty_nm STRING,
-  naics2_sector_cd STRING,
-  tri_ttl_rls_lbs_amt NUMERIC
+  state_cd            STRING,  -- 2-char state code
+  cnty_nm             STRING,  -- county name
+  state_cnty_fips_cd  STRING,  -- 5-digit county FIPS
+  naics2_sector_cd    STRING,  -- 2-digit NAICS sector
+  tri_ttl_rls_lbs_amt NUMERIC  -- total releases (lbs)
 );
